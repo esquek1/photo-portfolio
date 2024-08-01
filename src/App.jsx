f5538199b12a3ee0426543d937b0bbd2728bd655
kellz <esquejokelly@gmail.com>
2024-08-01 15:11:10 -0700

    Add video background

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import linkedInLogo from '/linkedin-logo.svg'
 import instagramLogo from '/instagram-logo.png'
 import './App.css'
@@ -7,36 +7,62 @@ import carWheel from '/86-wheel.jpg'
 import carTaillight from '/86-taillight.jpg'
 import carHeadlight from '/86-headlight.jpg'
 import carRolling from '/86-rolling.jpg'
+import datsun from '/datsun.jpg'
+import porsches from '/porsches.jpg'
+import porscheClassic from '/944S.jpg'
 
 function App() {
+  useEffect(() => {
+    const handleWheel = (event) => {
+      const container = document.querySelector('.whole-page');
+      if (container && event.deltaY !== 0) {
+        container.scrollLeft += event.deltaY;
+        event.preventDefault(); // Prevent default vertical scrolling behavior
+      }
+    };
 
+    window.addEventListener('wheel', handleWheel, { passive: false });
+
+    // Clean up the event listener on component unmount
+    return () => {
+      window.removeEventListener('wheel', handleWheel);
+    };
+  }, []);
 
   return (
-    <>
-      <div className='title'>
-        <p>Debbie Dicdican Photography</p>
-      </div>
+    <div className='whole-page'>
       
-      <div className='images'>
-        <img src={carRolling}></img>
-        <img src={carWheel}></img>
-        <img src={carTaillight}></img>
-        <img src={carHeadlight}></img>
+      <div className='sidebar'>
+        <div className='title'>
+          <p>Debbie Dicdican Portfolio</p>
+        </div>
+        
+        <div className='social'>
+          <a href="https://www.instagram.com/deldkyuubi/?hl=en" target="_blank">
+            <img src={instagramLogo} className="logo" alt="Instagram logo" />
+          </a>
+          <a href="https://www.linkedin.com/in/debbie-endena-dicdican-445845162/" target="_blank">
+            <img src={linkedInLogo} className="logo linkedin" alt="LinkedIn logo" />
+          </a>
+        </div>
         
       </div>
       
-      <div className='social'>
-        <a href="https://www.instagram.com/deldkyuubi/?hl=en" target="_blank">
-          <img src={instagramLogo} className="logo" alt="Instagram logo" />
-        </a>
-        <a href="https://www.linkedin.com/in/debbie-endena-dicdican-445845162/" target="_blank">
-          <img src={linkedInLogo} className="logo linkedin" alt="LinkedIn logo" />
-        </a>
-      </div>
+      <div className='images-container'> 
+        <div className='images'>
+        <img src={carRolling} alt="86 Rolling"/>
+          <img src={carWheel} alt="86 Wheel"/>
+          <img src={carTaillight} alt="86 Taillight"/>
+          <img src={carHeadlight} alt="86"/>
+          <img src={datsun} alt="Datsun 240z"/>
+          <img src={porscheClassic} alt="Porsche 944S"/>
+          <img src={porsches} alt="Porsches"/>
 
+        </div> 
+      </div>
       
       
-    </>
+    </div>
   )
 }
 
---
+++ src/components/sidebar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function sidebar() {
+  return (
+    <div>
+        <ol>
+            <li>About</li>
+            <li>Work</li>
+            <li>Contact</li>
+        </ol>
+    </div>
+  )
+}
+
+export default sidebar
