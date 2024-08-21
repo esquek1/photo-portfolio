8e1c2ad69c4c7bf3c40347815bf786a244fdc496
kellz <esquejokelly@gmail.com>
2024-08-21 15:06:14 -0700

    Add icons for footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,23 +1,50 @@
 import React from 'react'
 import { Link } from "react-router-dom";
 import "../css/footer.css"
+import { FaHome } from "react-icons/fa";
+import { FaInfoCircle } from "react-icons/fa";
+
+import { IoMdContact } from "react-icons/io";
+import { MdPhotoLibrary } from "react-icons/md";
+
 
 function Footer() {
   return (
     <div>
         <nav className='item'>
-            <ul className='nav-list'>
+            <ul className='nav-container'>
                 <li className='nav-item'>
-                    <Link to="/">Home</Link>
+                    <Link to="/">
+                        <div className='footer-icon'>
+                            <FaHome />
+                        </div>
+                        Home
+                    </Link>
                 </li>
                 <li className='nav-item'>
-                    <Link to="/about">About</Link>
+                    <Link to="/about">
+                        <div className='footer-icon'>
+                            <FaInfoCircle />
+                        </div>
+                        About
+                    </Link>
                 </li>
                 <li className='nav-item'>
-                    <Link to="/contact">Contact</Link>
+                    <Link to="/contact">
+                        <div className='footer-icon'>
+                            <IoMdContact />
+                        </div>
+                        Contact
+                    </Link>
                 </li>
                 <li className="nav-item extraBorder">
-                    <Link to="/work">Work</Link>
+                   
+                    <Link to="/work"> 
+                        <div className='footer-icon'>
+                            <MdPhotoLibrary />
+                        </div>
+                        Work
+                    </Link>
                 </li>
             </ul>
             
