423196e678d9f60f849f88feef63fb32d8c2bdab
kellz <esquejokelly@gmail.com>
2024-09-05 15:32:12 -0700

    Add input for contact page

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,21 +1,44 @@
 import React from 'react'
+import "../css/contact.css"
 
 function Contact() {
-  return (
-    <div className='contact-container'>
-      <div className='contact-item container-title'>
+	return (
+		<div className='contact-container'>
+			<div className='contact-item container-title'>
 
-      </div>
+			</div>
 
-      <div className='contact-item'>
-        For general questions and inquries
-        please fill out the contact form
-      </div>
-      <div className='contact-item'>
+			<div className='contact-item'>
+				For general questions and inquries
+				please fill out the contact form
+			</div>
+			<div className='contact-item contact-content'>
+				<label> 
+					Full Name 
+					<input type='' placeholder='First Name' />
+					<input type='' placeholder='Last Name' />
+				</label>
 
-      </div>
-    </div>
-  )
+				<br />
+				<label>
+					E-mail
+					<input type='email' placeholder='Email' />
+					<br />
+					example@example.com
+				</label>
+				<br/>
+				
+				<label>
+					Message
+					<input type='' placeholder='Message' />
+				</label>
+				<br/>
+				<button>
+					Send
+				</button>
+			</div>
+		</div>
+  	)
 }
 
 export default Contact
